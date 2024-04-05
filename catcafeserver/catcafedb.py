de4a011162e1f7af7b331fa7f82384704db5b480
imPr1x <113369739+imPr1x@users.noreply.github.com>
2024-04-05 13:20:52 -0600

    Modificacion de la db

--- catcafeserver/catcafedb.py
+++ catcafeserver/catcafedb.py
@@ -15,12 +15,21 @@ def create_tables():
             )
             """,
         
-        """CREATE TABLE IF NOT EXISTS users(
-            id_user INTEGER PRIMARY KEY AUTOINCREMENT,
+        """CREATE TABLE IF NOT EXISTS gatos(
+            id_gato INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            email TEXT NOT NULL
+            age INTEGER NOT NULL,
+            race TEXT NOT NULL
             )
-            """
+            """,
+            
+        """CREATE TABLE IF NOT EXISTS solicitudes(
+            id_solicitud INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_gato INTEGER,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            FOREIGN KEY(id_gato) REFERENCES gatos(id_gato)
+            )"""
     }
     
     db = get_db()
