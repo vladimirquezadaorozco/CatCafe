8fe998c7644dce66d385ea75d67e5f3d83ba02bc
imPr1x <113369739+imPr1x@users.noreply.github.com>
2024-04-05 09:05:53 -0600

    Agregamos la tabla de users
    Se creo la tabla users

--- catcafeserver/catcafedb.py
+++ catcafeserver/catcafedb.py
@@ -9,16 +9,26 @@ def get_db():
 def create_tables():
     tables = {
         """CREATE TABLE IF NOT EXISTS admins(
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_admin INTEGER PRIMARY KEY AUTOINCREMENT,
             user TEXT NOT NULL,
             password TEXT NOT NULL
             )
+            """,
+        
+        """CREATE TABLE IF NOT EXISTS users(
+            id_user INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL
+            )
             """
     }
+    
     db = get_db()
     cursor = db.cursor()
     for table in tables:
         cursor.execute(table)
+        
+
 
 
 create_tables()
